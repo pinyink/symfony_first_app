584465e3e69bbec0bf4f93355aab34d559c9dc9a
pinyink <nurkhafindi@gmail.com>
2024-02-02 13:14:18 +0700

    cru

--- src/Controller/SiswaController.php
+++ src/Controller/SiswaController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Entity\Siswa;
 use App\Form\SiswaType;
 use App\Repository\SiswaRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,11 +22,39 @@ class SiswaController extends AbstractController
         ]);
     }
 
-    #[Route(path: '/siswa_new', name: 'app_siswa_new', methods: ['GET'])]
-    public function new(): Response
+    #[Route(path: '/siswa_new', name: 'app_siswa_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm(SiswaType::class, new Siswa());
+        $siswa = new Siswa();
+        $form = $this->createForm(SiswaType::class, $siswa);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($siswa);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_siswa', [], Response::HTTP_SEE_OTHER);
+        }
+
         return $this->render('siswa/new.html.twig', [
+            'siswa' => $siswa,
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/siswa/{id}/edit', name: 'app_siswa_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Siswa $siswa, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(SiswaType::class, $siswa);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_siswa', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('siswa/edit.html.twig', [
+            'siswa' => $siswa,
             'form' => $form
         ]);
     }
