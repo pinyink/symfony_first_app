500c0e78dabc7c3fe6b7c25c5273590cb7954ceb
pinyink <nurkhafindi@gmail.com>
2024-02-20 09:51:58 +0700

    modify migration

--- migrations/Version20240201081035.php
+++ migrations/Version20240201081035.php
@@ -21,15 +21,11 @@ final class Version20240201081035 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE level (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(64) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, level_id INT NOT NULL, username VARCHAR(64) NOT NULL, password LONGTEXT NOT NULL, INDEX IDX_8D93D6495FB14BA7 (level_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D6495FB14BA7 FOREIGN KEY (level_id) REFERENCES level (id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D6495FB14BA7');
         $this->addSql('DROP TABLE level');
-        $this->addSql('DROP TABLE user');
     }
 }
