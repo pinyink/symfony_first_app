aee157b7a93b209ed20c5a71c26f150b25a42b5f
pinyink <nurkhafindi@gmail.com>
2024-03-10 11:34:06 +0700

    remove some lib from users

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,14 +9,11 @@ use App\Service\DataTableService;
 use App\Service\FileUploader;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\PasswordHasher\PasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
-use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/user'), IsGranted('ROLE_ADMIN')]
 class UserController extends AbstractController
