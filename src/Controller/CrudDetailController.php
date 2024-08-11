9dbeeaa3c6febc3c3c8e418e6a476093b44a0d5e
pinyink <nurkhafindi@gmail.com>
2024-08-11 19:07:48 +0700

    class metadata

--- src/Controller/CrudDetailController.php
+++ src/Controller/CrudDetailController.php
@@ -22,8 +22,28 @@ class CrudDetailController extends AbstractController
                 'Id Not Found For Id: '.$id
             );
         }
+
+        $class = $entitiyManager->getClassMetadata('Product');
+        $fields = [];
+        if (!empty($class->discriminatorColumn)) {
+            $fields[] = $class->discriminatorColumn['name'];
+        }
+        $fields = array_merge($class->getColumnNames(), $fields);
+        foreach ($fields as $index => $field) {
+            if ($class->isInheritedField($field)) {
+                unset($fields[$index]);
+            }
+        }
+        foreach ($class->getAssociationMappings() as $name => $relation) {
+            if (!$class->isInheritedAssociation($name)){
+                foreach ($relation['joinColumns'] as $joinColumn) {
+                    $fields[] = $joinColumn['name'];
+                }
+            }
+        }
         return $this->render('crud_detail/index.html.twig', [
             'crud' => $crud,
+            'fields' => $field
         ]);
     }
 
